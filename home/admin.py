b2dd3754fc5996d93d67b27524139ed0fb9c7521
Walidelmasri <waldaexist@gmail.com>
2024-03-08 01:40:12 +0000

    Final Commit

---
+++ home/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+    
+admin.site.register(BlogPost)
+admin.site.register(Comment)
+admin.site.register(Profile)
+admin.site.register(Carousel)
